1ab4dd150684c38a071cfb546bf0a959ed4686ae
ryhanhxx <rayhan328967@gmail.com>
2023-10-09 20:45:04 +0700

    add sdk

--- app/src/main/java/com/catnip/firebaseauthexample/presentation/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/catnip/firebaseauthexample/presentation/splashscreen/SplashActivity.kt
@@ -24,6 +24,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         checkIfUserLogin()
+
     }
 
     private fun checkIfUserLogin() {
